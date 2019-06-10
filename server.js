11b547679f5e1ed50c86a89dbc75c8c5a8b1b473
Glitch (wooden-kick-1) <none>
2019-06-10 11:47:07 +0000

    🕹📼 Checkpoint ./package.json:2466213/1315 ./watch.json:2466213/253 ./server.js:2466213/331

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
 
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
 // Declare Colors
 var rolesColors = [
   "dWhite",
